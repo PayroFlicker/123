be1d955a8f829484278f52cf5ace5ad13e58987e
PayroFlicker <arslan.efe1989@gmail.com>
2025-02-28 05:10:31 +0300

    ödev2

---
+++ wen/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'filecolor': '#ffeee7',
+        'textcolor': '#e96c40',
+        'textcolor2': '#995e64',
+        'textcolor3': "#89898b",
+        'textbgcolor': '#fee9e8',
+        'bordercolor': '#b2b1b7',
+      }
+    },
+  },
+  plugins: [],
+}
+
